161a6deaf988193cfef0c4add88de6ae341d4dae
akholomtseva <166552715+akholomtseva@users.noreply.github.com>
2024-06-19 20:27:10 +0000

    обработчики комманд

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -4,51 +4,28 @@ __all__ = [
 
 
 import logging
+
 from aiogram import Router, F
 from aiogram import types
 from aiogram.filters.command import Command
-from sqlalchemy import select, insert
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from config import TOKEN_URL
 from db import async_session_maker, User
-from .keyboards import keyboard_continue
-from .callbacks import callback_continue
-
-
-# настройка логирования
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
-
-# help_command
-help_str = """
-Вас приветствует бот <b><i>ИМЯ БОТА</i></b>\n
-💬 Вы можете вывести справочную информацию, отправив команду <b>/help</b>\n
-💬 Информацию о пользователе можно вывести с помощью команды <b>/status</b>
-"""
+from db.models import YandexDiskFolder
+from yandex import YandexFunctions
+from .callbacks import start_callback
+from .keyboards import registerbutton
 
 
 async def help_command(message: types.Message):
-    """справочная команда, регистрация пользователя"""
+    help_str = """Вас приветствует бот <b><i>Проверка обновлений яндекс диска</i></b>
+    💬 Регистрация пользователя <b>/start</b>
+    💬 Информацию о пользователе можно вывести с помощью команды <b>/status</b>"""
 
-    async with async_session_maker() as session:
-        session: AsyncSession
-        query = select(User).where(User.user_id == message.from_user.id)
-        user_exit = await session.execute(query)
-
-        if user_exit.scalars().all():
-            await message.reply(text=help_str, parse_mode="HTML")
-            logging.info(f"user {message.from_user.id} asks for help")
-
-        else:
-            new_user = {
-                "user_id": message.from_user.id,
-                "username": message.from_user.username
-            }
-            stmt = insert(User).values(**new_user)
-            await session.execute(stmt)
-            await session.commit()
-            await message.reply(help_str)
-            logging.info(f"register new user: {message.from_user.id}")
+    logging.info(f"user {message.from_user.id} asked for help")
+    await message.reply(text=help_str, parse_mode="HTML")
 
 
 async def status_command(message: types.Message):
@@ -56,19 +33,178 @@ async def status_command(message: types.Message):
 
     async with async_session_maker() as session:
         session: AsyncSession
-        query = select(User).where(User.user_id == message.from_user.id)
-        result = await session.execute(query)
-        user = result.scalar()
-        await message.reply(text=f"<b>User ID</b>: <i>{user.user_id}</i>\n"
-                                 f"<b>User name</b>: <i>{user.username}</i>",
-                                 parse_mode="HTML")
-        logging.info(f"user {message.from_user.id} is asking for status")
 
-    await message.reply("Хотите ли вы продолжить?", reply_markup=keyboard_continue)
+        user = await session.get(User, message.from_user.id)
+        await session.close()
+
+        if user is None:
+            await message.reply(text="Вы не зарегистрированы")
+        else:
+            text = f"""<b>Ваше имя</b>: <i>{user.name}</i>\n<b>Ваш ID</b>: <i>{user.id}</i>\n"""
+
+            await message.reply(text=text, parse_mode="HTML")
+
+        logging.info(f"user {message.from_user.id} requested status")
+
+
+async def start_command(message: types.Message):
+    async with async_session_maker() as session:
+        session: AsyncSession
+
+        user = await session.get(User, message.from_user.id)
+
+        await session.close()
+
+        if user is None:
+            await message.reply("Выберите роль:", reply_markup=registerbutton)
+        else:
+            await message.reply(f"Вы уже зарегестрированы")
+
+        logging.info(f"user {message.from_user.id} started the bot")
+
+
+async def register_user_command(message: types.Message):
+    id = int(message.text)
+    async with async_session_maker() as session:
+        session: AsyncSession
+
+        teacher = await session.get(User, id)
+
+        if teacher is None:
+            await message.answer("Преподаватель с таким ID не найден")
+        else:
+            new_user = User(id=message.from_user.id, user_teacher_id=id, name=message.from_user.username)
+            session.add(new_user)
+            await session.commit()
+            await session.close()
+
+            await message.answer(f"Вы зарегистрировались как слушатель")
+
+        logging.info(f"user {message.from_user.id} registered as a student")
+
+
+async def register_command(message: types.Message):
+    text = (f"Для регистрации токена следуйте шагам:\n"
+    f"1. Перейдите по ссылке: <a href=\"{TOKEN_URL}\">{TOKEN_URL}</a>\n"
+    f"2. Авторизируйтесь.\n"
+    f"3. Скопируйте ТОКЕН и вставьте его в <b>/token ТОКЕН</b>.")
+
+    await message.reply(text=text, parse_mode="HTML")
+
+    logging.info(f"user {message.from_user.id} requested token registration instructions")
+
+
+async def token_command(message: types.Message):
+    async with async_session_maker() as session:
+        session: AsyncSession
+
+        user = await session.get(User, message.from_user.id)
+
+        if user is None:
+            await message.reply("Для добавления токена зарегистрируйтесь как преподаватель")
+        elif user.user_teacher_id:
+            await message.reply("Вы не преподаватель")
+        else:
+            message_split = message.text.split()
+
+            if len(message_split) < 2:
+                if user.token:
+                    await message.reply(f"{user.token}")
+                else:
+                    await message.reply("Введите токен после команды через пробел /token")
+            else:
+                token = message_split[1].strip()
+
+                client = YandexFunctions(token=token)
+                yes = await client.check_token()
+
+                if yes:
+                    user.token = token
+                    await session.commit()
+
+                    await message.reply(f"Токен обновлен")
+                else:
+                    await message.reply(f"Неправильный токен")
+
+        logging.info(f"user {message.from_user.id} requested token command")
+
+
+async def add_command(message: types.Message):
+    async with async_session_maker() as session:
+        session: AsyncSession
+
+        message_split = message.text.split()
+
+        if len(message_split) < 2:
+            await message.reply("Укажите название или путь к папке через пробел после /add")
+        else:
+            name = message.text.split()[1].strip()
+
+            user = await session.get(User, message.from_user.id)
+
+            if not user:
+                await message.reply("Вы не зарегистрированы")
+            elif user.user_teacher_id:
+                await message.reply("Вы не преподаватель")
+            elif not user.token:
+                await message.reply("У Вас нет токена")
+            else:
+                new = YandexDiskFolder(user_teacher_id=user.id, name=name)
+                session.add(new)
+
+                await session.commit()
+                await session.close()
+
+                await message.reply(f"Папка '{name}' добавлена")
+
+        logging.info(f"user {message.from_user.id} asked for added a folder")
+
+
+async def delete_command(message: types.Message):
+    async with async_session_maker() as session:
+        session: AsyncSession
+
+        message_split = message.text.split()
+
+        if len(message_split) < 2:
+            await message.reply("Укажите название или путь к папке через пробел после /add")
+        else:
+            name = message.text.split()[1].strip()
+
+            user = await session.get(User, message.from_user.id)
+
+            if not user:
+                await message.reply("Вы не зарегистрированы")
+            elif user.user_teacher_id:
+                await message.reply("Вы не преподаватель")
+            elif not user.token:
+                await message.reply("У Вас нет токена")
+            else:
+                # ? поиск по совпадению айди и названию папки
+                stmt = select(YandexDiskFolder).filter(YandexDiskFolder.user_teacher_id is user.id, YandexDiskFolder.name is name)
+                result = await session.execute(stmt)
+                folder = result.scalars().first()
+
+                if not folder:
+                    await message.reply("Папка не найдена")
+                    return
+
+                await session.delete(folder)
+                await session.commit()
+
+                await message.reply(f"Папка '{name}' удалена")
+
+            logging.info(f"user {message.from_user.id} asking for deleted a folder")
 
 
 def register_message_handler(router: Router):
     """Маршрутизация"""
-    router.message.register(help_command, Command(commands=["start", "help"]))
+    router.message.register(start_command, Command(commands=["start"]))
+    router.message.register(register_command, Command(commands=["register"]))
     router.message.register(status_command, Command(commands=["status"]))
-    router.callback_query.register(callback_continue, F.data.startswith("continue_"))
+    router.message.register(token_command, Command(commands=["token"]))
+    router.message.register(help_command, Command(commands=["help"]))
+    router.message.register(add_command, Command(commands=["add"]))
+    router.message.register(delete_command, Command(commands=["delete"]))
+    router.message.register(register_user_command)
+    router.callback_query.register(start_callback, F.data.startswith("reg_"))
